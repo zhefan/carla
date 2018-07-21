392b6892c06017e73d6bd68303952e99b2d13a02
felipecode <felipe.alcm@gmail.com>
2018-07-20 17:22:37 -0700

    fixing the missing module

--- PythonClient/setup.py
+++ PythonClient/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='carla_client',
     version='0.8.4',
-    packages=['carla', 'carla.driving_benchmark', 'carla.agent',
+    packages=['carla', 'carla.driving_benchmark', 'carla.agent','carla.agent.modules',
               'carla.driving_benchmark.experiment_suites', 'carla.planner'],
     license='MIT License',
     description='Python API for communicating with the CARLA server.',
